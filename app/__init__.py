3e23bbe9aaf0696985263c67f54340615f8f46dc
Teddycxr <34271645+Teddycxr@users.noreply.github.com>
2020-03-04 13:25:03 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,23 +9,23 @@ app = Flask(__name__,
             template_folder='templates'  # 表示模板文件存放的目录
             )
 app.debug = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://elastos:workwork@192.168.1.5:3306/dab'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# app.config['SECRET_KEY'] = 'vaxaxaxv^%$@(*'  # 用来加密我们存储的数据
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@127.0.0.1:3306/zfj'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'vaxaxaxv^%$@(*'  # 用来加密我们存储的数据
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
 
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_SERVER'] = 'smtp.qq.com'
-app.config['MAIL_USERNAME'] = '735652082@qq.com'
-app.config['MAIL_PASSWORD'] = 'jahysoqfbtvpbbfc'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_SERVER'] = 'smtp.qq.com'
+# app.config['MAIL_USERNAME'] = '735652082@qq.com'
+# app.config['MAIL_PASSWORD'] = 'jahysoqfbtvpbbfc'
 
-app.config['CACHE_TYPE'] = 'redis'
-app.config['CACHE_REDIS_HOST']= '127.0.0.1'
-app.config['CACHE_REDIS_PORT']= 6379
-app.config['CACHE_REDIS_DB']= 0
-app.config['CACHE_REDIS_PASSWORD'] = ''
+# app.config['CACHE_TYPE'] = 'redis'
+# app.config['CACHE_REDIS_HOST']= '127.0.0.1'
+# app.config['CACHE_REDIS_PORT']= 6379
+# app.config['CACHE_REDIS_DB']= 0
+# app.config['CACHE_REDIS_PASSWORD'] = ''
 
 
 # cache = Cache(app,with_jinja2_ext=False)
